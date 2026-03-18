85bf7ecc60f20d5e4e38b9b3aa44a75e5dd0b6f8
jrabaye <j.rabaye@betacom.it>
2026-03-18 15:37:33 +0100

    aggiunto db-config.properties

--- EcomBike/src/main/java/com/betacom/ecombike/Application.java
+++ EcomBike/src/main/java/com/betacom/ecombike/Application.java
@@ -2,8 +2,10 @@ package com.betacom.ecombike;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.PropertySource;
 
 @SpringBootApplication
+@PropertySource("classpath:db-config.properties")
 public class Application {
 
 	public static void main(String[] args) {
