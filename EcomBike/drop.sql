422ae409de93e335d74462d0e06f90c13a040153
Betacom <Betacom@DESKTOP-TNM4LOQ.homenet.telecomitalia.it>
2026-03-18 15:17:27 +0100

    define drop

--- EcomBike/drop.sql
+++ EcomBike/drop.sql
@@ -1,15 +1,27 @@
 
     set client_min_messages = WARNING;
 
-    alter table if exists indirizzi 
-       drop constraint if exists FKsudfceq03yve264uvu8vxr1kr;
+    alter table if exists anagrafica 
+       drop constraint if exists FKbsc97wtuy3wnf5q9p2n7a44en;
+
+    alter table if exists dettaglio_ordine 
+       drop constraint if exists FKg8p950u9hg0towv49etgqvfo2;
+
+    alter table if exists indirizzo_spedizione 
+       drop constraint if exists FKehywytnkgipqu5b9fecs6nxb5;
 
     alter table if exists pagamento 
        drop constraint if exists FKlokfc657jetko2tqpe3y812ip;
 
-    drop table if exists indirizzi cascade;
+    drop table if exists anagrafica cascade;
+
+    drop table if exists dettaglio_ordine cascade;
+
+    drop table if exists indirizzo_spedizione cascade;
 
     drop table if exists messaggi_sistema cascade;
+
+    drop table if exists ordine cascade;
 
     drop table if exists pagamento cascade;
 
