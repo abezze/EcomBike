139648621928baf3e95ff953f7a83b85d36d9bea
Betacom <Betacom@DESKTOP-TNM4LOQ.homenet.telecomitalia.it>
2026-03-18 15:19:50 +0100

    script insert messaggi

---
+++ EcomBike/scriptInsert.sql
@@ -0,0 +1,21 @@
+INSERT INTO messaggi_sistema
+(lang, code, messaggio)
+VALUES('IT', 'rest_deleted', 'cancellato');
+INSERT INTO messaggi_sistema
+(lang, code, messaggio)
+VALUES('IT', 'dato_exist', 'Non esiste un dato con questo ID');
+INSERT INTO messaggi_sistema
+(lang, code, messaggio)
+VALUES('IT', 'rest_created', 'creato dato');
+INSERT INTO messaggi_sistema
+(lang, code, messaggio)
+VALUES('IT', 'rest_updated', 'modificato dato');
+INSERT INTO messaggi_sistema
+(lang, code, messaggio)
+VALUES('IT', 'category_change', 'Non è possibile cambiare categoria dato');
+INSERT INTO messaggi_sistema
+(lang, code, messaggio)
+VALUES('IT', 'login_invalid', 'Utente non valido');
+INSERT INTO messaggi_sistema
+(lang, code, messaggio)
+VALUES('IT', 'login_valid', 'Login eseguita');
