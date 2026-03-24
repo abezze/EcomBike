66c3dc7540ff2e3a20bc9798e9ea2b433702c062
Betacom <Betacom@DESKTOP-TNM4LOQ.homenet.telecomitalia.it>
2026-03-24 12:03:06 +0100

    dettaglio controller

---
+++ EcomBike/src/main/java/com/betacom/ecombike/controllers/DettaglioOrdineController.java
@@ -0,0 +1,109 @@
+package com.betacom.ecombike.controllers;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.betacom.ecombike.dto.inputs.DettaglioOrdineReq;
+import com.betacom.ecombike.response.Resp;
+import com.betacom.ecombike.services.interfaces.IDettaglioOrdineServices;
+import com.betacom.ecombike.services.interfaces.IMessaggioServices;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@RequiredArgsConstructor
+@RestController
+@RequestMapping("rest/dettaglioordine")
+public class DettaglioOrdineController {
+	
+	private final IDettaglioOrdineServices ordS;
+	private final IMessaggioServices    msgS;
+	
+	
+	
+	
+	
+	@PostMapping("/create")
+	public ResponseEntity<Resp> create(@RequestBody(required = true)  DettaglioOrdineReq req){
+		Resp r = new Resp();
+		HttpStatus status = HttpStatus.OK;
+		try {
+			ordS.create(req);
+			r.setMsg(msgS.get("rest_created"));
+		} catch (Exception e) {
+			r.setMsg(e.getMessage());
+			status = HttpStatus.BAD_REQUEST;
+		}
+		return ResponseEntity.status(status).body(r);		
+	}
+	
+	@PutMapping("/update")
+	public ResponseEntity<Resp> update(@RequestBody(required = true)  DettaglioOrdineReq req){
+		Resp r = new Resp();
+		HttpStatus status = HttpStatus.OK;
+		try {
+			ordS.update(req);
+			r.setMsg(msgS.get("rest_updated"));
+		} catch (Exception e) {
+			r.setMsg(e.getMessage());
+			status = HttpStatus.BAD_REQUEST;
+		}
+		return ResponseEntity.status(status).body(r);		
+	}
+
+	
+	
+	@DeleteMapping("/delete/{id}")
+	public ResponseEntity<Resp> delete(@PathVariable(required = true)  Long id){
+		Resp r = new Resp();
+		HttpStatus status = HttpStatus.OK;
+		try {
+			ordS.delete(id);
+			r.setMsg(msgS.get("rest_deleted"));
+		} catch (Exception e) {
+			r.setMsg(e.getMessage());
+			status = HttpStatus.BAD_REQUEST;
+		}
+		return ResponseEntity.status(status).body(r);		
+	}
+
+	
+
+	@GetMapping("/list")
+	public ResponseEntity<Object> list(){
+		Object r = new Object();
+		HttpStatus status = HttpStatus.OK;
+		try {
+			r= ordS.list();
+		} catch (Exception e) {
+			r=e.getMessage();
+			status = HttpStatus.BAD_REQUEST;
+		}
+		return ResponseEntity.status(status).body(r);
+		
+	}
+	@GetMapping("/findById")
+	public ResponseEntity<Object> findById (@RequestParam (required = true)  Long id){
+		Object r = new Object();
+		HttpStatus status = HttpStatus.OK;
+		try {
+			r= ordS.findById(id);
+		} catch (Exception e) {
+			r=e.getMessage();
+			status = HttpStatus.BAD_REQUEST;
+		}
+		return ResponseEntity.status(status).body(r);
+		
+	}
+
+}
